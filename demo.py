b61d71e555fc9f2820f7569564f603a7e36d576d
YuunqiLiu <liuyuunqi@163.com>
2025-02-11 11:23:41 +0800

    update import method.

--- demo.py
+++ demo.py
@@ -3,6 +3,6 @@ from pcpp import CmdPreprocessor
 
 p = CmdPreprocessor(['pcpp', '-o', 'output.sv',
                      '--passthru-defines', '--line-directive','', '--passthru-comments',
-                     '/home/liuyunqi/tree/ip_builder/input.sv'])
+                     'input.sv'])
 p.ip_build_prefix = 'DW_'
 p.run()
